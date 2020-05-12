8fa6acaf504b415b90fa49837a0100883cfeeff1
Ivan Milov <ivan.oyra.milov@gmail.com>
2020-05-12 14:38:58 +0400

    Init maven project

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import java.util.function.Function;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
